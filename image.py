c5697ca5213659e655430a9c8feae6caf651fd17
GeorgeN4E <67802984+GeorgeN4E@users.noreply.github.com>
2025-01-09 12:59:21 +0200

    Updated code

--- image.py
+++ image.py
@@ -3,7 +3,7 @@ import cv2
 import os
 
 #reading test image
-img = cv2.imread("p 0.jpg") #image name
+img = cv2.imread("PcYc0wV.png") #image name
 
 #reading label name from obj.names file
 with open(os.path.join("project_files",'obj.names'), 'r') as f:
